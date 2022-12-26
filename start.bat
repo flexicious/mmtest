3c57d73fc0a9cafd3bac6d3174fd777030525473
Flexicious <6350967+flexicious@users.noreply.github.com>
2022-12-26 12:20:47 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,11 @@
+@echo off
+
+set port=8082
+
+rem Find the process ID of the process listening on the specified port
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr /r ":%port% .*LISTENING"') do set pid=%%a
+
+rem Kill the process
+taskkill /F /PID %pid%
+
 npm i && npm start
