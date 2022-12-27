289a30cab298d7a7b6e7cde0ccd862d4a67f7f13
Flexicious <6350967+flexicious@users.noreply.github.com>
2022-12-26 21:26:14 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,12 +2,16 @@
 rem Get the first argument passed to the script
 set dbfile=%1
 
-set port=3333
+netstat -aon | findstr /r :3333
 
 rem Find the process ID of the process listening on the specified port
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr /r ":%port% .*LISTENING"') do set pid=%%a
-
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr /r :3333') do set pid=%%a
+echo killing %pid%
 rem Kill the process
+if defined pid (
 taskkill /F /PID %pid%
+) else (
+  echo No process found listening on port 3333
+)
 
 npm i && npm start %dbfile%
